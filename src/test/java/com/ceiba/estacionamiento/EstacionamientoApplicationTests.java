249bd4a6e8d56d4d2837ffb6b5151cfc90132799
Steven Calderon <scalderon1504@gmail.com>
2021-07-23 08:49:09 -0500

    first commit

---
+++ src/test/java/com/ceiba/estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
